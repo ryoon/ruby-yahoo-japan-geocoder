86b580d3d74ce8e677e8d8b8bef09d5508d40ddd
Ryo ONODERA <ryo@tetera.org>
2018-03-11 16:02:47 +0900

    Add 2018 to copyright notice

--- ruby-yahoo-geocoder.rb
+++ ruby-yahoo-geocoder.rb
@@ -3,7 +3,7 @@
 # Usage:
 # $ ruby24 ./ruby-yahoo-geocoder.rb input.txt output.txt
 #
-# Copyright (c) 2017 Ryo ONODERA <ryo@tetera.org>
+# Copyright (c) 2017, 2018 Ryo ONODERA <ryo@tetera.org>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
